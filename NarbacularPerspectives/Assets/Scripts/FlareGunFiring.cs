20e6040db6fd23884537cfddb74c0aee52992345
lakerpaxtan <paxtanlaker@berkeley.edu>
2020-04-25 12:12:27 -0700

    Added Flare gun, some setting stuff, fixed some camera issues, added color materials and physics materials, should be good to start portal stuff (1.5 hours)

---
+++ NarbacularPerspectives/Assets/Scripts/FlareGunFiring.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlareGunFiring : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource firingSound;
+    public int forceConst;
+    public GameObject flareBullet; 
+    void Start()
+    {
+        firingSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.rotation = Camera.main.transform.rotation;
+        //Checks every frame to see if you pressed fire (left click) while flare gun is enabled
+        if (Input.GetButtonDown("FireGun")){
+            fireFlareGun();
+            Debug.Log("firing flare gun");
+        }
+    }
+
+    // Fire the Gun using forces instead of raycasting. 
+    // The main idea is as follows: 
+    // Spawn Bullet in front of gun 
+    // Make sure Bullet's orientation is correct using Transform Rotations
+    // Add force in the direction in which you want the gun to 'fire' 
+    // Make sure bullet is set up with colliders correctly to detect impact! 
+    // (This requires some work in the bullet prefab script!)
+    // After you've done all your work, call fireFlareGunSounds() for fancy sounds!
+
+    //In your on collisionenter script, call the onHit from the target if a hit is detected.
+    void fireFlareGun(){
+        fireFlareGunSounds();
+        GameObject newBullet = GameObject.Instantiate(flareBullet, this.transform.position + 0.25f * this.transform.forward + 0.1f * this.transform.up, this.transform.rotation);
+        newBullet.transform.RotateAround(newBullet.transform.position, newBullet.transform.right, 90f);
+        newBullet.GetComponent<Rigidbody>().AddForce(forceConst * this.transform.forward);
+        Destroy(newBullet, 3f);
+
+    }
+
+    void fireFlareGunSounds(){
+        firingSound.Play();
+    }
+
+    
+}
---
+++ NarbacularPerspectives/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    public GameObject flareGun;
+  
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flareGun.SetActive(true);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+}
