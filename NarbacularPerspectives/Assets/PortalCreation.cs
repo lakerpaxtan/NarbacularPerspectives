ba690f19e3fe6276e28bbf1109b0993cdb624812
emilynguyen <emilyn@berkeley.edu>
2020-04-28 18:49:11 -0700

    can now create portals on different surfaces wit different sizes

--- NarbacularPerspectives/Assets/PortalCreation.cs
+++ NarbacularPerspectives/Assets/PortalCreation.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class PortalCreation : MonoBehaviour
 {
-    GameObject player;
+    public GameObject player;
+    public GameObject gm;
     LineRenderer rayLine;
     Ray ray;
     public GameObject portalShape;
     LineRenderer portalShapeLine;
     Vector3 topLeftCoord;
     Vector3 bottomRightCoord;
-    public Vector3 offset;
+    Vector3 norm;
+    public float offset;
 
     Portal a;
     Portal b;
+    public float size = 0.01f;
+    public float sizeSpeedIncrease;
     int numPortals;
 
     // Start is called before the first frame update
@@ -23,6 +27,7 @@ public class PortalCreation : MonoBehaviour
         //rayLine = gameObject.GetComponent<LineRenderer>();
         portalShapeLine = portalShape.GetComponent<LineRenderer>();
         numPortals = 0;
+        size = 1;
     }
 
     // Update is called once per frame
@@ -35,27 +40,42 @@ public class PortalCreation : MonoBehaviour
         if (Input.GetButtonDown("CreatePortal"))
         {
             portalShape.SetActive(true);
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hit, Mathf.Infinity))
+            if (Physics.Raycast(transform.position, Camera.main.ScreenPointToRay(Input.mousePosition).direction, out RaycastHit hit, Mathf.Infinity))
             {
-                portalShapeLine.transform.position = hit.point + new Vector3(0, 0, .1f);
+                Debug.DrawLine(transform.position, hit.point, Color.red);
+                norm = hit.normal.normalized;
+                portalShapeLine.transform.position = hit.point + norm * offset; 
                 //portalShapeLine.transform.rotation = hit.collider.transform.rotation;
             }
         }
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetButton("CreatePortal") && numPortals % 2 == 0)
+        {
+            size += sizeSpeedIncrease;
+            size %= 2;
+
+            portalShapeLine.SetPosition(0, new Vector3(-size, -size));
+            portalShapeLine.SetPosition(1, new Vector3(-size, size));
+            portalShapeLine.SetPosition(2, new Vector3(size, size));
+            portalShapeLine.SetPosition(3, new Vector3(size, -size));
+            portalShapeLine.SetPosition(4, new Vector3(-size, -size));
+        }
+
+        // Press B to create the portal at the indicated space 
+        if (Input.GetKeyDown(KeyCode.B)) 
         {
             if (numPortals % 2 == 0)
             {
-                a = new Portal(new Vector2(4, 4), new Vector2(0, 0), portalShapeLine.transform.position, new Vector3(0, 0, -1), player, "a" + Mathf.Floor(numPortals / 2));
+                a = new Portal(new Vector2(size, size), new Vector2(0, 0), portalShapeLine.transform.position, norm, player, "a" + Mathf.Floor(numPortals / 2));
             } else
             {
-                b = new Portal(new Vector2(4, 4), new Vector2(0, 0), portalShapeLine.transform.position, new Vector3(0, 0, -1), player, "b" + Mathf.Floor(numPortals / 2));
+                b = new Portal(new Vector2(size, size), new Vector2(0, 0), portalShapeLine.transform.position, norm, player, "b" + Mathf.Floor(numPortals / 2));
                 Portal.pairPortals(a, b);
+                gm.GetComponent<GameManager>().AddPortals(a, b);
             }
             numPortals++;
+            portalShape.SetActive(false);
         }
-
-
 
         //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
--- NarbacularPerspectives/Assets/Scripts/GameManager.cs
+++ NarbacularPerspectives/Assets/Scripts/GameManager.cs
@@ -10,32 +10,42 @@ public class GameManager : MonoBehaviour
     Portal testPortal;
     Portal testPortal2;
 
-    
-
+    List<Portal> portals;
 
     // Start is called before the first frame update
     void Start()
     {
         flareGun.SetActive(true);
-        testPortal = new Portal(new Vector2(4, 4), new Vector2(0, 0), new Vector3(-8f, 2, 14), new Vector3(0, 0, -1), playerObject, "testOne");
-        testPortal2 = new Portal(new Vector2(4, 4), new Vector2(0, 0), new Vector3(8f, 2, -16), new Vector3(0, 0, 1), playerObject, "testTwo");
+        portals = new List<Portal>();
 
+
+        //testPortal = new Portal(new Vector2(4, 4), new Vector2(0, 0), new Vector3(-8f, 2, 14), new Vector3(0, 0, -1), playerObject, "testOne");
+        //testPortal2 = new Portal(new Vector2(4, 4), new Vector2(0, 0), new Vector3(8f, 2, -16), new Vector3(0, 0, 1), playerObject, "testTwo");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Portal.pairPortals(testPortal, testPortal2);
+        //Portal.pairPortals(testPortal, testPortal2);
+        //portals.Add(testPortal);
+        //portals.Add(testPortal2);
         //testPortal.updateCameraRelativeToPlayer();
         //testPortal2.updateCameraRelativeToPlayer();
 
-        testPortal.update();
-        testPortal2.update();
+        foreach (Portal p in portals)
+        {
+            p.update();
+        }
 
         //testPortal.setTexture();
         //testPortal2.setTexture();
     }
 
+    public void AddPortals(Portal a, Portal b)
+    {
+        portals.Add(a);
+        portals.Add(b);
+    }
     
 
 }
